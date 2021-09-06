5c15d3c9f0a5c3f3e36064a6ea1bfd4842375a9b
PedroHLC <root@pedrohlc.com>
2021-09-06 13:30:43 -0300

    Delete head branch

--- repository.tf
+++ repository.tf
@@ -8,6 +8,7 @@ resource "github_repository" "github_management" {
   allow_merge_commit = false
   allow_squash_merge = true
   allow_rebase_merge = false
+  delete_branch_on_merge = true
   auto_init          = false
   license_template   = "mit"
   topics             = ["config", "terraform"]
