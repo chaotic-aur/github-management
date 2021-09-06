d5e27d23dbfcbb20809bff1fc379c72a074a6f1d
PedroHLC <root@pedrohlc.com>
2021-09-06 13:31:57 -0300

    Format

--- repository.tf
+++ repository.tf
@@ -2,24 +2,24 @@ resource "github_repository" "github_management" {
   name        = "github-management"
   description = "Terraform based repository to manage all our GutHub repositories"
 
-  private            = false
-  has_issues         = true
-  has_wiki           = false
-  allow_merge_commit = false
-  allow_squash_merge = true
-  allow_rebase_merge = false
+  private                = false
+  has_issues             = true
+  has_wiki               = false
+  allow_merge_commit     = false
+  allow_squash_merge     = true
+  allow_rebase_merge     = false
   delete_branch_on_merge = true
-  auto_init          = false
-  license_template   = "mit"
-  topics             = ["config", "terraform"]
+  auto_init              = false
+  license_template       = "mit"
+  topics                 = ["config", "terraform"]
 }
 
 resource "github_branch_protection" "team_baseline_config" {
-  repository     = github_repository.github_management.name
-  branch         = "main"
+  repository = github_repository.github_management.name
+  branch     = "main"
 
   required_status_checks {
-    strict = true
+    strict   = true
     contexts = ["atlas/mononoke/github-management", ]
   }
   required_pull_request_reviews {
